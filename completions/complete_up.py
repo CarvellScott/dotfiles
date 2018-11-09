94b2300a7c57c44d677d6c8034ca25c5bb047fd8
Carvell Scott <carvell.scott@gmail.com>
2018-11-08 20:59:54 -0600

    Added completion for up written in python to replace the one in bash.
    Also, its kind of hard to unit test this thing when its dependent on
    your current directory. I mean it can be done, but it's annoying.

--- completions/complete_up.py
+++ completions/complete_up.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 import pathlib
-import re
 from completion_helper import helper
-
-
-def _get_tags():
-    curr_path = pathlib.Path()
 
 
 def completion_hook(cmd, curr_word, prev_word, **kwargs):
@@ -16,31 +11,33 @@ def completion_hook(cmd, curr_word, prev_word, **kwargs):
     if "/" in prev_word:
         return matches
 
-    stems = curr_path.parts
+    parts = curr_path.parts[:-1]
+    # Map path parts to parent paths, prioritizing those in proximity.
     path_dict = {}
     for p in reversed(paths):
         path_dict[p.stem] = p
+
     if curr_word:
         # First complete a word that makes up a part of the path
-        if curr_word not in stems:
-            matches = [s for s in stems if s.startswith(curr_word)]
+        if curr_word not in parts:
+            matches = [s for s in parts if s.startswith(curr_word)]
+            # Return the path if we've narrowed it down.
             if len(matches) == 1:
                 matches = [str(path_dict[s]) for s in matches]
             else:
                 matches = [str(p) for p in paths if str(p).endswith(curr_word)]
+        # Return the path if there's an exact match in path_dict
         if curr_word in path_dict.keys():
-            matches = [path_dict[curr_word]]
+            matches = [str(path_dict[curr_word])]
     else:
-        matches = list(stems)
-        #return [k + ":" + str(v) for k, v in path_dict.items() if str(k).startswith(curr_word)]
-        #return [str(p) for p in paths if str(p).startswith(curr_word)]
+        matches = list(parts)
 
-    # Note the instances where I return the stems instead of the full paths.
     return matches
 
 
 def main():
     helper(completion_hook)
+
 
 if __name__ == "__main__":
     main()
