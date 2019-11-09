f3afcedb04001582a4c901bf2016ee77001b1206
Carvell Scott <carvell.scott@gmail.com>
2019-11-08 23:17:57 -0600

    Fixed a bug in which complete_up failed in deleted directories

--- completions/complete_up.py
+++ completions/complete_up.py
@@ -11,10 +11,17 @@ except (ImportError, ModuleNotFoundError):
 class CompleteUp(completion_utils.BashCompletion):
     def completion_hook(self, command, curr_word, prev_word):
         matches = []
-        curr_path = pathlib.Path().absolute()
+        try:
+            curr_path = pathlib.Path().absolute()
+        except FileNotFoundError:
+            curr_path = pathlib.Path.home().absolute()
+
+        paths = list(curr_path.parents)
+        if curr_path == pathlib.Path.home():
+            paths += [curr_path]
 
         # Greedy regexes will match the path closest to curr_path
-        paths = map(str, curr_path.parents)
+        paths = map(str, paths)
         regex = re.compile(r".*{}".format(re.escape(curr_word)))
         matches = {s for s in paths if regex.search(s)}
         return matches
