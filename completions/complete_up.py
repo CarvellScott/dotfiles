c653d65314cc5de263b5ed5f9e74ce645fb55449
Carvell Scott <carvell.scott@gmail.com>
2021-08-03 03:31:41 -0500

    Sadly realized that completion utils adds lots of import overhead

--- completions/complete_up.py
+++ completions/complete_up.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 import pathlib
 import re
-
-try:
-    import completion_utils
-except (ImportError, ModuleNotFoundError):
-    import df_completion_utils as completion_utils
+import sys
 
 
-class CompleteUp(completion_utils.BashCompletion):
-    def completion_hook(self, command, curr_word, prev_word):
-        matches = []
-        try:
-            curr_path = pathlib.Path().absolute()
-        except FileNotFoundError:
-            curr_path = pathlib.Path.home().absolute()
+def completion_hook(command, curr_word, prev_word):
+    matches = []
+    try:
+        curr_path = pathlib.Path().absolute()
+    except FileNotFoundError:
+        curr_path = pathlib.Path.home().absolute()
 
-        paths = list(curr_path.parents)
-        if curr_path == pathlib.Path.home():
-            paths += [curr_path]
+    paths = list(curr_path.parents)
+    if curr_path == pathlib.Path.home():
+        paths += [curr_path]
 
-        # Greedy regexes will match the path closest to curr_path
-        paths = map(str, paths)
-        regex = re.compile(r".*{}".format(re.escape(curr_word)))
-        matches = {s for s in paths if regex.search(s)}
-        return matches
+    # Greedy regexes will match the path closest to curr_path
+    paths = map(str, paths)
+    regex = re.compile(r".*{}".format(re.escape(curr_word)))
+    matches = {s for s in paths if regex.search(s)}
+    return matches
+
+def main():
+    results = completion_hook(*sys.argv[1:])
+    if len(results):
+        print("\n".join(results))
 
 
 if __name__ == "__main__":
-    CompleteUp().main()
+    main()
