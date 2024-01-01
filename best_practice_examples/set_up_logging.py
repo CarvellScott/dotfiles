b0777b70b6d5adcc46c9746ee4a5f14ee8858350
Carvell Scott <carvell.scott@gmail.com>
2024-01-01 14:48:53 -0600

    Improved logging practice by not using global variable

--- best_practice_examples/set_up_logging.py
+++ best_practice_examples/set_up_logging.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
 import logging
 
-_LOG = logging.getLogger(__name__)
 
-def set_up_logging():
+def add_quickfix_handler(logger):
+    fmt = '%(pathname)s:%(lineno)d: [%(levelname)-8s] %(message)s'
+    formatter = logging.Formatter(fmt)
+    handler = logging.FileHandler("quickfix.txt", "w")
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+def set_up_logging(logger):
     logging_config = {
-        "format": "%(asctime)s [%(levelname)s] %(module)s:%(lineno)d:%(message)s",
+        "format": "%(asctime)s [%(levelname)-8s] %(filename)s:%(lineno)d: %(message)s",
         "datefmt": "%F %T"
     }
     logging.basicConfig(**logging_config)
-    if False:
-        quickfix_handler = logging.FileHandler("quickfix.txt", "w")
-        fmt = '%(pathname)s:%(lineno)d:%(message)s'
-        formatter = logging.Formatter(fmt, logging_config["datefmt"])
-        quickfix_handler.setFormatter(formatter)
-        quickfix_handler.setLevel(logging.WARNING)
-        _LOG.addHandler(quickfix_handler)
-    _LOG.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
+
+def main():
+    log = logging.getLogger(__name__)
+    set_up_logging(log)
+    add_quickfix_handler(log)
+    log.info("Logging has been set up")
+    log.warning("Something has gone wrong that may lead to an error.")
+    log.error("An error has occurred, but the program is still able to run")
+    try:
+        raise Exception("This is an exception.")
+    except Exception as e:
+        log.exception("An exception was intercepted.")
+    log.fatal("If you see this, something catastrophic has occurred.")
+    log.debug("This is debug level, probably shouldn't have too many of these")
 
 if __name__ == "__main__":
-    set_up_logging()
-    _LOG.info("Logging has been set up")
-    _LOG.warning("Something has gone wrong that may lead to an error.")
-    _LOG.error("An error has occurred, but the program is still able to run")
-    _LOG.fatal("If you see this, something catastrophic has occurred.")
-    _LOG.debug("This is debug level, probably shouldn't have too many of these")
-
+    main()
