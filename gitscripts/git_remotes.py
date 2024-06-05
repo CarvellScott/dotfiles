2f1e56135da04692ec1fe9ff151875de83deb7d1
Carvell Scott <carvell.scott@gmail.com>
2024-06-05 01:04:19 -0500

    Rewrote git_remotes.py to be less redundant

--- gitscripts/git_remotes.py
+++ gitscripts/git_remotes.py
@@ -1,38 +1,27 @@
 #!/usr/bin/env python3
 import subprocess
 
-def get_remotes():
-    p = subprocess.run(
-        "git remote show",
-        stdout=subprocess.PIPE,
-        shell=True,
+def _get_remotes():
+    output = subprocess.check_output(
+        ("git", "remote", "show"),
         universal_newlines=True
     )
-    remote_names = p.stdout.splitlines()
-    return remote_names
+    for line in output.splitlines():
+        yield line
 
 
 def remote_to_url(remote_name):
-    p = subprocess.run(
-        "git remote get-url {}".format(remote_name),
-        stdout=subprocess.PIPE,
-        shell=True,
+    output = subprocess.check_output(
+        ("git", "remote", "get-url", remote_name),
         universal_newlines=True
     )
-    url = p.stdout.strip()
-    return remote_name, url
-
-
-def display_remote_urls():
-    remote_names = map(remote_to_url, get_remotes())
-    remote_names = map(lambda i: "{}: {}".format(*i), remote_names)
-    remote_names = list(remote_names)
-    remote_names.sort()
-    print("\n".join(remote_names))
+    url = output.strip()
+    return f"{remote_name}: {url}"
 
 
 def main():
-    display_remote_urls()
+    for _ in sorted(_get_remotes()):
+        print(remote_to_url(_))
 
 
 if __name__ == "__main__":
