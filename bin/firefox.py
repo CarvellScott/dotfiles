d76b06b150fbf49b05fc97ba3a1cbb2d33534f16
Carvell Scott <carvell.scott@gmail.com>
2024-08-25 23:31:19 -0500

    Fixed a bug where firefox would not open youtube urls with their parameters.

--- bin/firefox.py
+++ bin/firefox.py
@@ -4,6 +4,7 @@
 # executables is broken.
 import subprocess
 import sys
+import shlex
 
 def open_firefox(*args):
     str_args = list(map(str, *args))
@@ -17,6 +18,13 @@ def open_firefox(*args):
 
 def main():
     firefox_executable = "/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
+    subprocess_args = sys.argv
+    # Make sure urls with parameters are properly quoted
+    for i, arg in enumerate(sys.argv):
+        if i == 0:
+            continue
+        subprocess_args[i] = shlex.quote(arg)
+
     open_firefox([firefox_executable] + sys.argv[1:])
 
 if __name__ == "__main__":
