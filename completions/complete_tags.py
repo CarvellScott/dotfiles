81a993b0bb9615e610ff5314786d12bef68510bd
Carvell Scott <carvell.scott@gmail.com>
2025-01-31 01:00:16 -0600

    Added Tentative support for zsh in complete_tags.py
    tbh I just went and made bash the default shell on my macbook before I
    finished testing this. Not even sure if it works with zsh.

--- completions/complete_tags.py
+++ completions/complete_tags.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
+import os
 import pathlib
 import re
+import shlex
 import sys
-
-try:
-    import completion_utils
-except (ImportError, ModuleNotFoundError):
-    import df_completion_utils as completion_utils
 
 
 def _get_tags():
@@ -24,19 +21,36 @@ def _get_tags():
     # If the tags file exists, open it and build a sorted list of tags.
     if tags_path.exists():
         with open(tags_path, "r") as f:
+            regex = re.compile(r"[^\t]+\t")
             lines = f.readlines()
             lines = filter(lambda i: not i.startswith("!"), lines)
-            lines = map(lambda i: re.match(r"[^\t]+\t", i).group(0), lines)
+            lines = map(lambda i: regex.match(i).group(0), lines)
             lines = map(lambda i: i.strip(), lines)
-            tags = list(set(lines))
-            tags.sort()
+            tags = lines
     return tags
 
 
-class CompleteTags(completion_utils.BashCompletion):
-    def completion_hook(self, cmd, curr_word, prev_word):
-        matches = [k for k in _get_tags() if k.startswith(curr_word)]
-        return matches
+def completion_hook(command, curr_word, prev_word):
+    for tag in _get_tags():
+        if tag.startswith(curr_word):
+            yield tag
+
+
+def main():
+    shell = os.environ.get("SHELL")
+    results = []
+    if "bash" in shell:
+        results = completion_hook(*sys.argv[1:])
+
+    if "zsh" in shell:
+        command = os.environ.get("COMP_LINE", "")
+        command_split = shlex.split(command) + [""]
+        curr_word = command_split[int(os.environ.get("COMP_CWORD", 0))]
+        results = completion_hook(command, curr_word, None)
+
+    for result in results:
+        print(result)
+
 
 if __name__ == "__main__":
-    CompleteTags().main()
+    main()
